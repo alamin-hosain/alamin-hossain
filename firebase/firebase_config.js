b4a09ddabb183d805805f37b78914e1005acf35d
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-12-10 23:10:40 +0600

    firsr commit

---
+++ firebase/firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3Pc_D1n2xWZVCNCNiM3Q_De3_h4C14kI",
+    authDomain: "alaminhossain-a5863.firebaseapp.com",
+    projectId: "alaminhossain-a5863",
+    storageBucket: "alaminhossain-a5863.appspot.com",
+    messagingSenderId: "234309274390",
+    appId: "1:234309274390:web:187011616756fd14651e37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
